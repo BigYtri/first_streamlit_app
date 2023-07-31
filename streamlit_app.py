cf2b601c275536833e043e97ff83875c9a0580a1
BigYtri <141013423+BigYtri@users.noreply.github.com>
2023-07-31 15:55:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("Esta es la primera prueba")
 
@@ -7,4 +10,4 @@ streamlit.text('🥣Omega 3 y avena con arándanos')
 streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔Huevo de gallinas camperas hervidas')
 streamlit.text('🥑🍞Advocado Toast')
-
+streamlit.dataframe(my_fruit_list)
