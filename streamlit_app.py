42049b9bb95c10dc24d3b37d681b385d8112456c
BigYtri <141013423+BigYtri@users.noreply.github.com>
2023-07-31 10:05:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("Esta es la primera prueba")
 
-streamlit.header('Menú de desayuno')
+streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 streamlit.text('🥣Omega 3 y avena con arándanos')
 streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔Huevo de gallinas camperas hervidas')
