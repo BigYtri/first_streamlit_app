84d4f64a5a50e8770665b8512e74493a165f7f6a
BigYtri <141013423+BigYtri@users.noreply.github.com>
2023-07-31 16:02:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
+
 streamlit.title("Esta es la primera prueba")
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
@@ -11,9 +16,7 @@ streamlit.text('🥣Omega 3 y avena con arándanos')
 streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔Huevo de gallinas camperas hervidas')
 streamlit.text('🥑🍞Avocado Toast')
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display the table on the page.
