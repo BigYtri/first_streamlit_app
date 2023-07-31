86d6662edd5db02dd6b0925228731f73aaf3f961
BigYtri <141013423+BigYtri@users.noreply.github.com>
2023-07-31 10:04:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title("Esta es la primera prueba")
 
 streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo de gallinas camperas hervidas')
+streamlit.text('🥣Omega 3 y avena con arándanos')
+streamlit.text('🥗Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔Huevo de gallinas camperas hervidas')
+streamlit.text('🥑🍞Advocado Toast')
 
